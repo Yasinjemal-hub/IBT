7fb006f6e3f40ec9db7f346f2043daea2811c2ab
Yasingemale-hub <yasin.ugr-8504-16@aau.edu.et>
2026-07-13 14:48:03 +0300

    add tuple, dictionery , list and module

---
+++ module1/yasin.py
@@ -0,0 +1,67 @@
+# print ("hello world")
+# age=20
+# weight=50
+# student_name="yasin"
+# is_active=True
+# balance=8000
+# salary=int(input("Enter your salary: "))
+# print(f"{student_name} is {age} years old")
+# print(student_name.upper())
+# print(len(student_name))
+# print(student_name.strip())
+# print("B" in student_name)
+# print(salary + 1000)
+
+# if balance >=10000:
+#     print("you can buy")
+# else:
+#     print( "you can't buy")
+# balance=10
+# while balance>0:
+#     balance-=1
+#     print(f"i have {balance} remainign")
+
+# def myFunction():
+#     balance=10
+# while balance>0:
+#     balance-=1
+#     print(f"i have {balance} remainign")
+# myFunction()
+
+# bill = 100
+# people=5["almaz", "abebe", "eyasu","fuad" ]
+# def divider():
+#     pay= bill/people
+#     tip=10
+#     return pay + tip
+# divider()
+# print(divider())
+# customer = {
+# "name": "Almaz Bekele",
+# "balance": 1500, # ETB
+# "city": "Addis Ababa",
+# }
+# customer["name"] # "Almaz Bekele"
+# customer["balance"] = 2000 
+# customer["phone"]=933766338
+
+# print(customer)
+
+# duplicates vanish
+# nums = [1, 2, 2, 3, 3, 3]
+# unique = set(nums) # {1, 2, 3}
+# members = {"Almaz", "Dawit"}
+# members.add("Tigist")
+# print(members) # {"Almaz", "Dawit", "Tigist"}
+# print(unique)    
+
+# col_1={1,2,3,4}
+# col_2={5,3,7,2,9}
+# city_set=set(col_1 - col_2)
+# print(city_set)
+
+import math
+
+pi=math.pow(5,2)
+
+print(pi)
