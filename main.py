e9799a795ac2efbc7669d7c59ceb48db56df951b
Yasingemale-hub <yasin.ugr-8504-16@aau.edu.et>
2026-07-13 15:21:50 +0300

    we add try and except error handling method

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-with open("note.tex",'a') as f:
-     print(f.write("\"barvo"))
+# try:
+#     with open("note.tex",'a') as f:
+#      print(f.write("\"barvo"))
+# except FileExistsError:
+#    print("file not found")
 
+
+try:
+      amount = int(input("Amount: "))
+      result = 1000 / amount
+except ValueError:
+    print("Please enter a number")
+except ZeroDivisionError:
+      print("Amount can't be zero")
+finally:
+    print("done")
