6a86b571de121868b7e8a971ef15b66f7d948f2f
Yasingemale-hub <yasin.ugr-8504-16@aau.edu.et>
2026-07-15 15:13:06 +0300

    add savingacoount class

--- module1/Day5/account.py
+++ module1/Day5/account.py
@@ -1,19 +1,23 @@
 class account:
-    def __init__(self,owner, balance):
+    def __init__(self,owner, balance, rate=0.07):
        self.balance=balance
        self.owner=owner
+       self.rate=rate
     def deposit(self,amount):
         return self.balance + amount
-
+    def statment(self):
+        return self.balance +self.balance * self.rate
 class savingaccount(account):
-     def __init__(self, owner, balance):
+     def __init__(self, owner, balance, ):
          super().__init__(owner, balance)
 alazar=savingaccount("alazar",2000)
 
 class checkingaccount(account):
-    def __init__(self, owner, balance):
-        super().__init__(owner, balance)
+    def __init__(self, owner, balance,rate ):
+        super().__init__(owner, balance, rate)
+     
 
 Rahmet=checkingaccount("Rahmet",1800)
 
 print(Rahmet.balance)
+print(Rahmet.deposit(700))
