9c14907319247635d1133ad2ccba48c5068d1dad
Yasingemale-hub <yasin.ugr-8504-16@aau.edu.et>
2026-07-15 15:30:45 +0300

    add acc-type

--- module1/Day5/account.py
+++ module1/Day5/account.py
@@ -1,23 +1,27 @@
 class account:
-    def __init__(self,owner, balance, rate=0.07):
+    def __init__(self,owner, balance,acc_type, rate=0.07,):
        self.balance=balance
        self.owner=owner
        self.rate=rate
+       self.acc_type=acc_type
     def deposit(self,amount):
         return self.balance + amount
     def statment(self):
-        return self.balance +self.balance * self.rate
+        if self.acc_type=="saving":
+          return self.balance +self.balance * self.rate
+        else:
+            return self.balance
 class savingaccount(account):
-     def __init__(self, owner, balance, ):
-         super().__init__(owner, balance)
-alazar=savingaccount("alazar",2000)
+     def __init__(self, owner, balance, acc_type):
+         super().__init__(owner, balance, acc_type)
+alazar=savingaccount("alazar",2000, "saving")
 
 class checkingaccount(account):
-    def __init__(self, owner, balance,rate ):
-        super().__init__(owner, balance, rate)
+    def __init__(self, owner, balance,acc_type):
+        super().__init__(owner, balance,acc_type)
      
 
-Rahmet=checkingaccount("Rahmet",1800)
+Rahmet=checkingaccount("Rahmet",1800, "saving")
 
 print(Rahmet.balance)
 print(Rahmet.deposit(700))
