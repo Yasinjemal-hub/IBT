317d196f68711f21dae29bd0f47d9f570aaf86a9
Yasingemale-hub <yasin.ugr-8504-16@aau.edu.et>
2026-07-20 15:17:44 +0300

    add linear DSA method O(1)

--- module1/Day8/Dsa2.py
+++ module1/Day8/Dsa2.py
@@ -1,4 +1,16 @@
-C="Yasin"
-D="Yasin"
+# C="Yasin"
+# D="Yasin"
 
-print(C is D)
+# print(C is D)
+
+
+
+def linear_search(item, target):
+    for i, x in enumerate(item):
+        if x == target:
+            print(i) 
+    return -1 
+
+balances = [1500, 800, 2300, 450]
+linear_search(balances, 450) 
+
