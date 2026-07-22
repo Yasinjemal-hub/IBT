4d9d5f1bc0e6bbae1efa18758bf32f5b131c8315
Yasingemale-hub <yasin.ugr-8504-16@aau.edu.et>
2026-07-22 14:06:01 +0300

    merge sort array

--- module1/Day8/Dsa2.py
+++ module1/Day8/Dsa2.py
@@ -5,12 +5,26 @@
 
 
 
-def linear_search(item, target):
-    for i, x in enumerate(item):
-        if x == target:
-            print(i) 
-    return -1 
+# def linear_search(item, target):
+#     for i, x in enumerate(item):
+#         if x == target:
+#             print(i) 
+#     return -1 
 
-balances = [1500, 800, 2300, 450]
-linear_search(balances, 450) 
+# nums = [450,90,800,750]
+# linear_search(nums, 450) 
 
+
+# def binary_search(item, target):
+#  lo,hi=0, len(item) -1
+def binary_search(items, target):
+    lo, hi = 0, len(items) - 1
+    while lo <= hi:
+         mid = (lo + hi) // 2
+         if items[mid] == target:
+                print (mid)
+         elif items[mid] < target:
+            lo = mid + 1 # go right
+         else:
+            hi = mid - 1 # go left
+    return -1
