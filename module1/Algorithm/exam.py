45bfce173cfa2abc10bb398d1c57c09e71194e27
Yasingemale-hub <yasin.ugr-8504-16@aau.edu.et>
2026-07-24 14:15:44 +0300

    reverse compare challange

--- module1/Algorithm/exam.py
+++ module1/Algorithm/exam.py
@@ -2,10 +2,19 @@
 #       ○ Test 1: getOnlyEvens([1, 2, 3, 6, 4, 8]) prints [ 4]
 #       ○ Test 2: getOnlyEvens([0, 1, 2, 3, 4]) prints [0, 2, 4]
 #  Test1
-def getOnlyEvens(arr):
-    
-    result = [val for idx, val in enumerate(arr) if idx % 2 == 0 and val % 2 == 0]
-    print(result)
 
-getOnlyEvens([1, 2, 3, 6, 4, 8])  
-getOnlyEvens([0, 1, 2, 3, 4])
+
+# Question 2
+# ● Create a function that takes a two-digit number as an parameter and prints "Ok" inthe console if the given string is greater than its reversed digit version. If not, the function will print "Not ok"
+#      ○ Test 1: reverseCompare(72) prints "ok" because 72 > 27
+#      ○ reverseCompare(23) prints "Not ok", because 23 is not greater than 32
+
+def reverseCompare(num):
+    if num>27:
+        print("ok")
+    else:
+        print("not ok")
+reverseCompare(72)
+reverseCompare(23)
+
+ 
