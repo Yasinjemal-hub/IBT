ca56ec485f890a530d0596ec96b0b92f123e4552
Yasingemale-hub <yasin.ugr-8504-16@aau.edu.et>
2026-07-24 14:24:11 +0300

    return Factorial

--- module1/Algorithm/exam.py
+++ module1/Algorithm/exam.py
@@ -9,13 +9,29 @@
 #      ○ Test 1: reverseCompare(72) prints "ok" because 72 > 27
 #      ○ reverseCompare(23) prints "Not ok", because 23 is not greater than 32
 
-def reverseCompare(num):
-    reversed_num = int(str(num)[::-1])
-    if num>reversed_num:
-        print("ok")
-    else:
-        print("not ok")
-reverseCompare(72)
-reverseCompare(23)
+# def reverseCompare(num):
+#     reversed_num = int(str(num)[::-1])
+#     if num>reversed_num:
+#         print("ok")
+#     else:
+#         print("not ok")
+# reverseCompare(72)
+# reverseCompare(23)
 
- 
+#  Question 3
+# ● Write a function that takes a positive integer and returns the factorial of the number. Notes: The factorial of 0 is 1. Ex: factorial seven is : 1 × 2 × 3 × 4 × 5 × 6 × 7. The factorial of any positive integer x is x * (x - 1) * (x - 2) * . . . . . . * 1 (ex: factorial of 4 is 4 * 3 * 2 * 1 = 24)
+#     ○ Test 1: returnFactorial(5) outputs 120
+#     ○ Test 2: returnFactorial(6) outputs 720
+#     ○ Test 3: returnFactorial(0) outputs 1
+def returnFactorial(n):
+    if n == 0 or n == 1:
+        return 1
+    
+    factorial = 1
+    for i in range(2, n + 1):
+        factorial *= i
+    return factorial
+
+print(returnFactorial(5))
+print(returnFactorial(6))
+print(returnFactorial(0))
