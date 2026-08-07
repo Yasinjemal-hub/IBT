ce90a0691b82039fed097a1845aaa2802fe59bf3
Yasingemale-hub <yasin.ugr-8504-16@aau.edu.et>
2026-08-07 14:43:36 +0300

    add brace

--- module2/Day_06/class_excersise/js/script.js
+++ module2/Day_06/class_excersise/js/script.js
@@ -11,6 +11,24 @@
 // let name="ahmed"
 
 // name="suliman"
-let num="abbd"
+// let num="abbd"
 
-console.log(typeof Number(num))
+// console.log(typeof Number(num))
+
+
+// const total = 1200; // ETB
+// const isMember = true;
+// // free delivery over 1000 OR member
+// const freeDelivery =
+// total >= 1000 || isMember; // true
+// const bigMember = total > 1000 && isMember;
+
+
+const name = 'Almaz';
+const total = 480;
+// old way — clumsy + joins
+
+// template literal — readable\
+console.log(`Hi ${name}, pay ${total} ETB`)
+`Hi ${name}, pay ${total} ETB`
+// "Hi Almaz, pay 480 ETB"
