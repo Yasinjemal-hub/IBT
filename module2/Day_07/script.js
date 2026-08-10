cde25ceb1a0ba464144c22c592129eac043d79b1
Yasingemale-hub <yasin.ugr-8504-16@aau.edu.et>
2026-08-10 14:04:00 +0300

    inner function "closes over" city

--- module2/Day_07/script.js
+++ module2/Day_07/script.js
@@ -14,17 +14,29 @@
 
 //rest is operater parameter define .... with array []
 
-//declaration
-function fun(){
-    console.log("hello")
-}
+// //declaration
+// function fun(){
+//     console.log("hello")
+// }
 
-// expression
-const name=function fun(){
-    console.log("hello")
-} 
+// // expression
+// const name=function fun(){
+//     console.log("hello")
+// } 
 
 //arrow
 
-const arr=(x)=> x*15
-arr(30)
+// const arr=(x)=> x*15
+// arr()
+
+// console.log(arr(3))
+
+// function makeGreeter(city) {
+// // inner function "closes over" city
+// return function (name) {
+// return `Selam ${name}, from ${city}`;
+// };
+// }
+// const addis = makeGreeter("Addis Ababa");
+// addis("Almaz")
+// console.log(addis("Almaz"))
