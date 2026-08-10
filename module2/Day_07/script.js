ee6e6700ace098e42571a01834be844d82138cd9
Yasingemale-hub <yasin.ugr-8504-16@aau.edu.et>
2026-08-10 13:32:31 +0300

    function declaration and expression

---
+++ module2/Day_07/script.js
@@ -0,0 +1,23 @@
+// function greet(...param){
+//  console.log(`hi ${params}`)
+// }
+
+// greet() function declaration
+
+// const greet=function greet(...param){
+//  console.log(`hi ${params}`)
+// }
+
+// greet() function exprssion
+
+//parameter is placeholder
+
+//rest is operater parameter define .... with array []
+
+function fun(){
+    console.log("hello")
+}
+
+const name=function fun(){
+    console.log("hello")
+}
