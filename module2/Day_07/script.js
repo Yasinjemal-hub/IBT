bcacf34332a52629de492fb3c0182cc934f23c1f
Yasingemale-hub <yasin.ugr-8504-16@aau.edu.et>
2026-08-10 13:41:15 +0300

    arrow function

--- module2/Day_07/script.js
+++ module2/Day_07/script.js
@@ -14,10 +14,17 @@
 
 //rest is operater parameter define .... with array []
 
+//declaration
 function fun(){
     console.log("hello")
 }
 
+// expression
 const name=function fun(){
     console.log("hello")
-}
+} 
+
+//arrow
+
+const arr=(x)=> x*15
+arr(30)
