63c7bebc4d83bc156eae299ca52dbb0f751840c3
Yasingemale-hub <yasin.ugr-8504-16@aau.edu.et>
2026-08-11 14:36:01 +0300

    add day 9 folder

--- module2/Day_07/script.js
+++ module2/Day_07/script.js
@@ -40,3 +40,68 @@
 // const addis = makeGreeter("Addis Ababa");
 // addis("Almaz")
 // console.log(addis("Almaz"))
+
+// function makeQueue() {
+// let number = 0; // private
+// return {
+// next() { number++; return number; },
+// current() { return number; },
+// };
+// }
+// const bank = makeQueue(); // CBE counter
+// bank.next(); // 1 (Almaz)
+// bank.next(); // 2 (Dawit)
+// bank.current(); // 2
+
+// function mukera(){
+//  let num=3
+
+// return function mchmer( x=1){
+//  console.log(x+=num)
+// }
+
+// }
+
+// const wetet=mukera()
+// wetet()
+
+
+// function myfun(num1, num2, fun){
+//  return fun(num1,num2)
+// }
+
+// const adder=(num1,num2)=> num1 + num2
+
+
+
+// console.log(myfun(35,67,adder))
+
+const subtotal=(...price)=>{
+    let total=0;
+    for(price of prices){
+        total+=price
+    }
+    return total
+}
+
+const discountby=(rate)=>{
+    return price * rate
+}
+
+const withVat =(n)=>{
+    return n *1.15
+}
+
+const toETB=(n)=>{
+    return n.toFixed(2)
+}
+
+function makeReceiptMaker(){
+    let orderNo=0;
+    const memeberoff=discountby(0.1)
+
+    return function(...item){
+        orderNo++
+        subtotal()
+    }
+}
---
+++ module2/Day_08/script.js
@@ -0,0 +1,8 @@
+let array=["apple", "banana"[1,2,3]]
+
+
+let obj={
+    key:"vlaue"
+}
+
+console.log(obj)
