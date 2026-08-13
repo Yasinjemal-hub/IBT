fcf29b142a366f8e82f25d2125a1b3a962441443
Yasingemale-hub <yasin.ugr-8504-16@aau.edu.et>
2026-08-13 15:52:42 +0300

    parameter with default and rest

--- module2/Day_07/practice/script.js
+++ module2/Day_07/practice/script.js
@@ -1,29 +1,59 @@
-// declare and calling a function
-// define once
-function addVat(amount) {
-return amount * 1.15; // 15% VAT
+// // declare and calling a function
+// // define once
+// function addVat(amount) {
+// return amount * 1.15; // 15% VAT
+// }
+// // call many times
+// addVat(480); // 552
+// addVat(1000); // 1150
+
+
+// function sum(num){
+//  return num + 30
+// }
+
+// console.log(sum(80))
+// console.log(sum(70))
+
+// // function declaration & expression with hioisted
+// // declaration — hoisted, callable above
+// greet(); // ✓ works
+// function greet() {
+// return "Selam!";
+// }
+
+// // expression — NOT hoisted
+// const greet = function () {
+// return "Selam!";
+// };
+// greet(); // call AFTER the line
+
+// parameter defualt $ reset
+
+// default value when none passed
+function deliveryFee(total, rate = 0.05) {
+return total * rate;
 }
-// call many times
-addVat(480); // 552
-addVat(1000); // 1150
+deliveryFee(1000); // 50 (uses 0.05)
+deliveryFee(1000, 0.10); // 100
+// rest — collect many args into an array
+function totalBill(...prices) {
+let sum = 0;
+for (const p of prices) sum += p;
+return sum;
+}
+; // 480
+console.log(totalBill(120, 200, 160))
 
+function totalpay(...pays){
+    let sum=0;
+    for(const p of pays)
+        sum +=p;
+        return sum
+  
 
-function sum(num){
- return num + 30
 }
 
-console.log(sum(80))
-console.log(sum(70))
+console.log(totalpay(200,760,780))
 
-// function declaration & expression with hioisted
-// declaration — hoisted, callable above
-greet(); // ✓ works
-function greet() {
-return "Selam!";
-}
-
-// expression — NOT hoisted
-const greet = function () {
-return "Selam!";
-};
-greet(); // call AFTER the line
+// arrow function
