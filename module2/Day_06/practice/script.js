697b61c3e7a3cd4ceceeb9fa4a913c71ada50a6b
Yasingemale-hub <yasin.ugr-8504-16@aau.edu.et>
2026-08-13 11:10:06 +0300

    converting and inspecting

--- module2/Day_06/practice/script.js
+++ module2/Day_06/practice/script.js
@@ -13,3 +13,14 @@ let chosen; // undefined
 const middle = null;
 
 //converter and inspecting
+
+typeof 480; // "number"
+typeof 'birr'; // "string"
+// input from a form is ALWAYS a string
+const raw = '480'; // from <input>
+Number(raw) + 20; // 500 ✓
+raw + 20; // '48020' ✗
+String(480); // "480"
+Boolean(0); // false
+
+//
