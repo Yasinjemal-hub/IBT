f631412f314c993561d9897d3f50a7a89e4ed46c
Yasingemale-hub <yasin.ugr-8504-16@aau.edu.et>
2026-08-13 15:37:20 +0300

    function declaration and calling

---
+++ module2/Day_07/practice/script.js
@@ -0,0 +1,16 @@
+// declare and calling a function
+// define once
+function addVat(amount) {
+return amount * 1.15; // 15% VAT
+}
+// call many times
+addVat(480); // 552
+addVat(1000); // 1150
+
+
+function sum(num){
+ return num + 30
+}
+
+console.log(sum(80))
+console.log(sum(70))
