f7caffe6716d108e1124528192155c8ff1ec9e50
Yasingemale-hub <yasin.ugr-8504-16@aau.edu.et>
2026-08-14 09:41:26 +0300

    creating array and accessing

---
+++ module2/Day_08/practice/script.js
@@ -0,0 +1,11 @@
+const menu = ['Doro Wat', 'Tibs', 'Shiro'];
+menu[0]; // "Doro Wat" (zero-based)
+menu.length; // 3
+menu[menu.length - 1]; // last item
+menu.push("Firfir"); // add to end
+menu.pop(); // remove from end
+menu.includes("Tibs"); // true
+menu.indexOf("Shiro"); // 2
+menu.shift()
+menu.unshift("karya")
+console.log(menu)
