71949a0f803f15d7056539d7221cb3c4ee153ed6
Yasingemale-hub <yasin.ugr-8504-16@aau.edu.et>
2026-08-17 10:32:09 +0300

    reading content and value

--- module2/Day_09/practice/script.js
+++ module2/Day_09/practice/script.js
@@ -7,3 +7,16 @@ const first = document.querySelector(".item");
 const items =
 document.querySelectorAll(".item");
 items.length; //
+
+//reding content and value
+
+const h1 = document.querySelector("h1");
+h1.textContent; // "Addis Market"
+// form field value
+const input = document.querySelector("#qty");
+input.value; // "3" (always a string!)
+Number(input.value); // 3
+// attributes & data-*
+const li = document.querySelector(".item");
+li.getAttribute("class"); // "item"
+li.dataset.id; // data-id="7"
